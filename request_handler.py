3daf7abf8ad897d28b1454ee8d73093f5c751f9a
Daun Kim <yennakim11@gmail.com>
2024-04-16 17:07:24 -0500

    able to post/create animals

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer
+from views import create_animal
 import json
 
 
@@ -109,11 +110,26 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(new_animal).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
-from .animal_requests import get_all_animals, get_single_animal
+from .animal_requests import get_all_animals, get_single_animal, create_animal
 from .location_requests import get_all_locations, get_single_location
 from .employee_requests import get_all_employees, get_single_employee
 from .customer_requests import get_all_customers, get_single_customer

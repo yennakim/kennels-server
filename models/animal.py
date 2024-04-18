7f682c7dd47b2e4c38dce6907b3136b261f1774a
Daun Kim <yennakim11@gmail.com>
2024-04-17 23:51:08 -0500

    refactored to match with the ANIMALS dict properties

--- models/animal.py
+++ models/animal.py
@@ -3,10 +3,10 @@ class Animal():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, name, breed, status, location_id, customer_id):
+    def __init__(self, id, name, species, status, location_id, customer_id):
         self.id = id
         self.name = name
-        self.breed = breed
+        self.species = species
+        self.locationId = location_id
+        self.customerId = customer_id
         self.status = status
-        self.location_id = location_id
-        self.customer_id = customer_id
